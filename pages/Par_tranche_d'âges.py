3e01a9e8ffe0fc23eab87fad736cd35fa6b62aac
Christine <156078562+christine2377@users.noreply.github.com>
2024-05-07 14:54:43 +0200

    Update Par_tranche_d'âges.py

--- pages/Par_tranche_d'âges.py
+++ pages/Par_tranche_d'âges.py
@@ -2,41 +2,41 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import pandas as pd
-import openpyxl
 import streamlit as st
 
 st.markdown("# Par tranche d'âges")
 st.sidebar.markdown("# Par tranche d'âges")
 
-fichier = "Tranche age.xlsx"
+fichier = "BOB2024-Dashboard.xlsx"
+sheet = "Tranche_ages"
 
 # Quelles sont les raisons de votre engagement bénévole aujourd'hui dans cette cette association ?
-table = pd.read_excel( fichier, sheet_name = "Feuil2" ,skiprows=3,nrows= 8, index_col =0, dtype = "object")
+table = pd.read_excel( fichier, sheet_name = sheet ,skiprows=3,nrows= 8, index_col =0, dtype = "object")
 table = table.applymap(lambda x: f'{x * 100:.0f}%')
 
 
 #Vous diriez, à propos de votre engagement dans cette association, qu'il est synonyme, avant tout, de :
-table1 = pd.read_excel( fichier, sheet_name = "Feuil2" ,skiprows=17,nrows= 6, index_col =0, dtype = "object")
+table1 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=17,nrows= 6, index_col =0, dtype = "object")
 table1 = table1.applymap(lambda x: f'{x * 100:.0f}%')
 
 
 #Vous avez le sentiment que votre activité bénévole, vous permet : Plusieurs réponses possible
-table2 = pd.read_excel( fichier, sheet_name = "Feuil2" ,skiprows=30,nrows= 8, index_col =0, dtype = "object")
+table2 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=30,nrows= 8, index_col =0, dtype = "object")
 table2 = table2.applymap(lambda x: f'{x * 100:.0f}%')
 
 
 #Si vous éprouvez des déceptions, sur quels thèmes portent-elles ? Plusieurs réponses possibles
-table3 = pd.read_excel( fichier, sheet_name = "Feuil2" ,skiprows=46,nrows= 8, index_col =0, dtype = "object")
+table3 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=46,nrows= 8, index_col =0, dtype = "object")
 table3 = table3.applymap(lambda x: f'{x * 100:.0f}%')
 
 
 #Quelles seraient vos attentes pour bien vivre votre activité bénévole ? Plusieurs réponses possibles
-table4 = pd.read_excel( fichier, sheet_name = "Feuil2" ,skiprows=60,nrows= 9, index_col =0, dtype = "object")
+table4 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=60,nrows= 9, index_col =0, dtype = "object")
 table4 = table4.applymap(lambda x: f'{x * 100:.0f}%')
 
 
 # Attendez-vous de votre association qu'elle vous aide à développer ces savoir-être ?
-table5 = pd.read_excel( fichier, sheet_name = "Feuil2" ,skiprows=78,nrows= 6, index_col =0, dtype = "object")
+table5 = pd.read_excel( fichier, sheet_name = sheet ,skiprows=78,nrows= 6, index_col =0, dtype = "object")
 table5 = table5.applymap(lambda x: f'{x * 100:.0f}%')
 
 
