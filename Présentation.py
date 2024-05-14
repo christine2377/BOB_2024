4d00ec78c2381fc48c6612ce079d0d36a57bce31
Christine <156078562+christine2377@users.noreply.github.com>
2024-05-14 12:33:33 +0200

    Update Présentation.py

--- Présentation.py
+++ Présentation.py
@@ -18,8 +18,8 @@ st.sidebar.markdown("# Présentation")
 ("Enquête en ligne du 15 février au 22 avril 2024, auprès de 3 717 bénévoles de profils et d'horizons diversifiés. "
  "Cet échantillon robuste permet de réaliser des analyses croisées et de tenir compte de la diversité des situations et des formes d'engement.")
 
-("Les résultats téléchargeables ici, par secteur d’activités et par tranche d’âge, viennent en complément de ceux présentés dans La France bénévole "
- "2024, en ligne sur www.recherches-solidarites.org")
+("En cliquant dans le menu à gauche vous pouvez télécharger les résultats par secteur d'activités et par tranche d'âges. Ils viennent en complément de ceux présentés dans *La France bénévole* "
+ "*2024*, en ligne sur www.recherches-solidarites.org")
 
 ("D’autres résultats sont disponibles, sur demande, auprès de [Marie](mailto:marie.duros@recherches-solidarites.org).")
 
